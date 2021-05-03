7cc0fda7733954c195b47bf3b4546cd574323beb
sharon2719 <otiennosharon@gmail.com>
2021-05-03 09:18:57 +0300

    first commit

---
+++ trial.py
@@ -0,0 +1,30 @@
+"""The Female Conductor
+---------------------
+"""
+
+loop = 1
+while (loop < 3):
+    noun = input("Pick a name: ")
+    noun1 = input ("Choose a noun: ")
+    pronoun = input("Choose a pronoun: ")
+    pronoun1 = input("Choose pronoun1: ")
+    plural_noun = input("Pick a plural noun: ")
+    state= input("Pick a state: ")
+    country = input("Name a country: ")
+    adjective = input("Give an adjective: ")
+    adjective1 = input(" Give an adjective1: ")
+    noun2 = input("Name the law: ")
+    noun3 = input("Give the army name: ")
+
+
+    print("----------")
+    print(f"{noun} is one of the most influential women in Black History. {pronoun} was a conductor for The Underground Rail Road in the late 18th Century.")
+    print(f"{plural_noun} treated the {noun1} with such great remorse and viewed them as barbaric and always told them that they were their property..")
+    print(f"When {noun} was denied a chance for freedom by her master,{pronoun},ran away and fled 100 miles to {state} where she met with the head of Anti-Slavery.")
+    print(f"{pronoun} went for over 13 missions and rescued over 70 slaves including her family members before the {noun2} was passed.")
+    print(f"When the Civil War in {country} began ,{pronoun} became the first woman to lead an army in war.{pronoun} was a spy and scout for the {noun3}.")
+    print()
+    print(f"After {noun} death in 1913 ,she became such an icon of great {adjective} and {adjective1} for women")
+    print(f"{pronoun1} last words were","I am going to prepare a place for you.")
+    print("----------")
+    loop+=1
